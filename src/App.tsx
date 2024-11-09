0f2a7d38387b255f07092447921a2e157787bbdf
Woojin Sim <58705148+WoojinSim@users.noreply.github.com>
2024-11-10 00:26:52 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/woojin-portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
