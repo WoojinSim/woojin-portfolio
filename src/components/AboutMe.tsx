2e00205e16a83caca4a827fddc9852b458280257
Woojin Sim <ssqwe2@gmail.com>
2024-11-18 07:41:21 +0900

    11.18 업데이트
    * 문구수정

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -15,7 +15,7 @@ const AboutMe: React.FC = () => {
         오늘날, 코딩이라는 수단으로 아이디어를 실현하고, 만들어진 결과물들이 불러오는 변화들이 너무나도 소중해졌습니다.
       </span>
       <span css={contentLabel}>
-        저는, 더욱더 완성도 높은 결과물 빗어내기 위해 노력하고 있는 <strong>심우진</strong>입니다.
+        저는, 더욱더 완성도 높은 결과물 빚어내기 위해 노력하고 있는 <strong>심우진</strong>입니다.
       </span>
     </div>
   );
